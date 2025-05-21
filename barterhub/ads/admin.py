f06a9120ae83a98414e280a3f6113b76b6106af4
Pavel Anikin <PavelDidko@gmail.com>
2025-05-21 22:31:44 +0300

    add views, urls

--- barterhub/ads/admin.py
+++ barterhub/ads/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Ad, ExchangeProposal
 
-# Register your models here.
+@admin.register(Ad)
+class AdAdmin(admin.ModelAdmin):
+    list_display = ("title", "category", "condition", "user", "created_at")
+    search_fields = ("title", "description")
+    list_filter = ("category", "condition")
+
+@admin.register(ExchangeProposal)
+class ExchangeProposalAdmin(admin.ModelAdmin):
+    list_display = ("ad_sender", "ad_receiver", "status", "created_at")
+    search_fields = ("comment",)
+    list_filter = ("status",)
--- barterhub/ads/models.py
+++ barterhub/ads/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+User = get_user_model()
+
+
+class Ad(models.Model):
+    CONDITION_CHOICES = [
+        ('new', 'Новое'),
+        ('used', 'Б/у'),
+    ]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='ads')
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    image_url = models.CharField(max_length=255, blank=True, null=True)
+    category = models.CharField(max_length=100)
+    condition = models.CharField(max_length=10, choices=CONDITION_CHOICES)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.title} ({self.condition})'
+
+
+class ExchangeProposal(models.Model):
+    STATUS_CHOICES = [
+        ("pending", "Ожидает"),
+        ("accepted", "Принято"),
+        ("rejected", "Отклонено"),
+    ]
+
+    ad_sender = models.ForeignKey(Ad, related_name="sent_proposals", on_delete=models.CASCADE)
+    ad_receiver = models.ForeignKey(Ad, related_name="received_proposals", on_delete=models.CASCADE)
+    comment = models.TextField(blank=True)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default="pending")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Обмен {self.ad_sender} на {self.ad_receiver} — {self.status}"
